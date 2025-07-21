60b0504c5f15baa54fb32ccfdeec5ee793194120
Debanjan <debanjanofficial2023@gmail.com>
2025-07-21 11:01:28 +0530

    hi

--- week-6-experiments/src/App.jsx
+++ week-6-experiments/src/App.jsx
@@ -3,34 +3,19 @@ import React, { useState } from "react";
 let counter = 4;
 
 function App() {
-
-  return(
+  return (
     <div>
       <CardWrapper>
-        hi there,this is 1st children of cardwrapper
+        <CardWrapper>hi there,this is 1st children of cardwrapper</CardWrapper>
+        <CardWrapper>hi there,this is 2nd children of cardwrapper</CardWrapper>
+        <CardWrapper>hi there,this is 3rd children of cardwrapper</CardWrapper>
+        <CardWrapper>hi there,this is 4th children of cardwrapper</CardWrapper>
       </CardWrapper>
-      <CardWrapper>
-        hi there,this is 2nd children of cardwrapper
-      </CardWrapper>
-      <CardWrapper>
-        hi there,this is 3rd children of cardwrapper
-      </CardWrapper>
-      <CardWrapper>
-        hi there,this is 4th children of cardwrapper
-      </CardWrapper>
-
     </div>
-  )
-
-  
+  );
 }
 
-
-
-function CardWrapper({children}){
-
-return(
-  <div style={{border: "2px solid black"}}>{children}</div>
-);
+function CardWrapper({ children }) {
+  return <div style={{ border: "2px solid black" }}>{children}</div>;
 }
 export default App;
