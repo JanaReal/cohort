7fb9ad8b4479e347be3d6b2a68dbeb23f8c2da62
Debanjan <debanjanofficial2023@gmail.com>
2025-07-21 11:43:28 +0530

    prev-update

--- week-6.2-experiments/src/App.jsx
+++ week-6.2-experiments/src/App.jsx
@@ -3,18 +3,16 @@ import { useState , useEffect} from "react";
 function App(){
   let [id,setId] = useState(1);
 
-  function goToPage(id){
-    setId(id);
-  }
+  
 
 
 
   return (
     <div>
-      <button onClick={()=> goToPage(1)}> 1 </button>
-      <button onClick={()=> goToPage(2)}> 2 </button>
-      <button onClick={()=> goToPage(3)}> 3 </button>
-      <button onClick={()=> goToPage(4)}> 4 </button>
+      <button onClick={()=> setId(1)}> 1 </button>
+      <button onClick={()=> setId(2)}> 2 </button>
+      <button onClick={()=> setId(3)}> 3 </button>
+      <button onClick={()=> setId(4)}> 4 </button>
       <Todo id = {id}/>;
     </div>
   )
