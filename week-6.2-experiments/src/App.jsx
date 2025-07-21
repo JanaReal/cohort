21858703008761a05261758f5314374700e213d0
Debanjan <debanjanofficial2023@gmail.com>
2025-07-21 11:33:47 +0530

    state with button

--- week-6.2-experiments/src/App.jsx
+++ week-6.2-experiments/src/App.jsx
@@ -1,31 +1,41 @@
 import { useState , useEffect} from "react";
 
 function App(){
+  let [id,setId] = useState(1);
 
-  let [todos,setTodos] = useState(0);
-
-  useEffect(()=>{
-    fetch("https://sum-server.100xdevs.com/todos")
-      .then(async function(res){
-        const json = await res.json();
-        setTodos(json.todos);
-      })
-  },[])
+  function goToPage(id){
+    setId(id);
+  }
 
 
 
   return (
     <div>
-      {todos.map(todo => <Todo key={todo.id} title={todo.title} description={todo.description}/>)};
+      <button onClick={()=> goToPage(1)}> 1 </button>
+      <button onClick={()=> goToPage(2)}> 2 </button>
+      <button onClick={()=> goToPage(3)}> 3 </button>
+      <button onClick={()=> goToPage(4)}> 4 </button>
+      <Todo id = {id}/>;
     </div>
   )
 }
 
-function Todo(title, description){
+function Todo({id}){
+
+  let [todos,setTodos] = useState(0);
+
+  useEffect(()=>{
+    fetch(`https://sum-server.100xdevs.com/todo?id=${id}`)
+      .then(async function(res){
+        let json = await res.json();
+        setTodos(json.todos)
+      })
+  },[])
+
   return(
     <div>
-      <h1>{title}</h1>
-      <h4>{description}</h4>
+      <h1>{todos.title}</h1>
+      <h4>{todos.description}</h4>
     </div>
   )
 }
