fccdd3f0e74c46cff5be9ec813fdce5302166fa4
Debanjan <debanjanofficial2023@gmail.com>
2025-07-23 18:41:23 +0530

    prop-drilling elimination by useContext

--- week-7.1/src/App.jsx
+++ week-7.1/src/App.jsx
@@ -1,23 +1,50 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { lazy, Suspense } from "react";
+import { lazy, Suspense,useState } from "react";
+import { CountContext } from "./context";
+import { useContext } from "react";
 const Dashboard = lazy(() => import("./components/Dashboard"));
 const Landing = lazy(() => import("./components/Landing"));
-import { Topbar } from "./components/Navbar";
+//import { Topbar } from "./components/Navbar";
 
 function App() {
+  const[count,setCount] = useState(0);
   return (
     <div>
-      <BrowserRouter>
-        <Topbar />
-        <Suspense>
-          <Routes>
-            <Route path="/landing" Component={Landing}></Route>
-            <Route path="/dashboard" Component={Dashboard}></Route>
-          </Routes>
-        </Suspense>
-      </BrowserRouter>
+      <CountContext.Provider value={count}>
+      <Count setCount={setCount}/>
+      </CountContext.Provider>
+      
+      
     </div>
   );
+}
+function Count({setCount}){
+  return <div>
+    <CountRenderer/>
+    <Buttons setCount={setCount}/>
+  </div>
+
+}
+
+function CountRenderer(){
+  const count = useContext(CountContext);
+  return <div>
+    {count}
+  </div>
+}
+
+function Buttons({setCount}){
+  const count = useContext(CountContext);
+  return <div>
+    <button onClick={()=>{
+      setCount( count + 1)
+    }}>Increase</button>
+    
+    <button onClick={()=>{
+      setCount( count - 1)
+    }}>Decrease</button>
+  </div>
+
 }
 
 export default App;
