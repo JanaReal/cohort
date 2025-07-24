98d344fbb39a7b915e776ee38f96f102daf7ef6c
Debanjan <debanjanofficial2023@gmail.com>
2025-07-24 19:52:03 +0530

    useContext,recoil

--- week-7.1/src/App.jsx
+++ week-7.1/src/App.jsx
@@ -1,48 +1,57 @@
-
-
-import { RecoilRoot, useRecoilState, useRecoilValue } from "recoil";
-import { countAtom } from "./store/atoms/count";
-
-
+import { RecoilRoot, useSetRecoilState, useRecoilValue } from "recoil";
+import { countAtom, isOddEvenSelector } from "./store/atoms/count";
 
 function App() {
-  
   return (
     <div>
       <RecoilRoot>
-        <Count/>
+        <Count />
       </RecoilRoot>
-      
     </div>
   );
 }
-function Count(){
-  return <div>
-    <CountRenderer/>
-    <Buttons />
-  </div>
-
+function Count() {
+  console.log("re-render");
+  return (
+    <div>
+      <CountRenderer />
+      <Buttons />
+      <IsEvenOdd />
+    </div>
+  );
 }
 
-function CountRenderer(){
+function CountRenderer() {
   const count = useRecoilValue(countAtom);
-  return <div>
-    {count}
-  </div>
+  return <div>{count}</div>;
 }
 
-function Buttons(){
-  const [count,setCount] = useRecoilState(countAtom);
-  return <div>
-    <button onClick={()=>{
-      setCount( count + 1)
-    }}>Increase</button>
-    
-    <button onClick={()=>{
-      setCount( count - 1)
-    }}>Decrease</button>
-  </div>
+function IsEvenOdd() {
+  const isEven = useRecoilValue(isOddEvenSelector);
+  return <div>{isEven? "it is even" : "it is odd"}</div>;
+}
 
+function Buttons() {
+  const setCount = useSetRecoilState(countAtom);
+  return (
+    <div>
+      <button
+        onClick={() => {
+          setCount((count) => count + 1);
+        }}
+      >
+        Increase
+      </button>
+
+      <button
+        onClick={() => {
+          setCount((count) => count - 1);
+        }}
+      >
+        Decrease
+      </button>
+    </div>
+  );
 }
 
 export default App;
--- week-7.1/src/store/atoms/count.jsx
+++ week-7.1/src/store/atoms/count.jsx
@@ -1,6 +1,15 @@
-import { atom } from 'recoil';
+import { atom, selector } from 'recoil';
 
 export const countAtom = atom({
   key: 'countAtom',
   default: 0,
 });
+
+export const isOddEvenSelector = selector({
+  key:'isOddEvenSelector',
+  get: ({get}) => {
+    const count = get(countAtom);
+    return count % 2 == 0;
+  },
+  
+})
