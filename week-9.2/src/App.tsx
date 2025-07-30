5469280516e38cf04d2838ba4978d1dc53dd915e
Debanjan <debanjanofficial2023@gmail.com>
2025-07-30 20:06:09 +0530

    hm

--- week-9.2/src/App.tsx
+++ week-9.2/src/App.tsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+function App(){
+  return <>
+  <Todo title={"go to gym"} desc={"at 6pm"} done={false}/>
+  </>
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
 }
 
-export default App
+interface TodoProp {
+  title : string,
+  desc : string,
+  done: boolean
+}
+
+function Todo(prop : TodoProp){
+  return <div>
+    <h1>{prop.title}</h1>
+    <h2>{prop.desc}</h2>
+    <h3>{prop.done}</h3>
+  </div>
+}
+
+export default App;
