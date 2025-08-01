0c85e1e4e0f1d8766555bcc3f3b233d111716cdf
Debanjan <debanjanofficial2023@gmail.com>
2025-08-01 20:26:55 +0530

    inserUser/updateUser

---
+++ week-10.2/dist/index.js
@@ -0,0 +1,30 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const index_1 = require("../src/generated/prisma/index");
+const prisma = new index_1.PrismaClient();
+async function insertUser(email, password, firstName, lastName) {
+    const res = await prisma.user.create({
+        data: {
+            email,
+            password,
+            firstName,
+            lastName
+        }
+    });
+    console.log(res);
+}
+async function updateUser(username, { firstName, lastName }) {
+    const res = await prisma.user.update({
+        where: {
+            email: username
+        },
+        data: {
+            firstName,
+            lastName
+        }
+    });
+    console.log(res);
+}
+//insertUser("debanjan","0412","d","j");
+updateUser("debanjan", { firstName: "deb", lastName: "jana" });
+//# sourceMappingURL=index.js.map
---
+++ week-10.2/src/index.ts
@@ -0,0 +1,42 @@
+import { PrismaClient } from '../src/generated/prisma/index';
+const prisma = new PrismaClient();
+
+
+async function insertUser(email: string, password: string, firstName: string, lastName: string) {
+  const res = await prisma.user.create({
+    data: {
+        email,
+        password,
+        firstName,
+        lastName
+    }
+  })
+  console.log(res);
+}
+
+
+
+interface UpdateParams {
+    firstName: string;
+    lastName: string;
+}
+
+async function updateUser(username: string, {
+    firstName,
+    lastName
+}: UpdateParams) {
+  const res =await prisma.user.update({
+    where:{
+        email: username
+    },
+    data: {
+        firstName,
+        lastName
+    }
+  })
+  console.log(res);
+}
+
+
+//insertUser("debanjan","0412","d","j");
+updateUser("debanjan",{firstName:"deb",lastName:"jana"});
