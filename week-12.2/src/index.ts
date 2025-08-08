87bc7c06e4f2e112e929b1f12cfb1f210439d9ad
Debanjan <debanjanofficial2023@gmail.com>
2025-08-08 19:59:30 +0530

    Record api

--- week-12.2/src/index.ts
+++ week-12.2/src/index.ts
@@ -1,4 +1,17 @@
-interface User{
+
+type Users = Record<string,{age:number; name:  string}>;
+
+const users: Users = {
+    "123":{age:21,name:"harkirat"},
+    "121":{age:32,name:"naman"}
+}
+
+
+
+
+
+/*
+ interface User{
     //id: string;
     name: String;
     age: number;
@@ -11,7 +24,7 @@ const user1: Readonly<User>={
     age:32
 }
 user1.age = 34;
-/*
+
 type UpdatedProps = Pick<User, 'name'| 'email'| 'password'>
 
 type UpdatedPropsOptional = Partial<UpdatedProps>;
